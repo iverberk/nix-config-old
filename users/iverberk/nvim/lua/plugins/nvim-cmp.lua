f886ce65bfd86b0001f7196a39111152bd50e64b
Ivo Verberk <ivo.verberk@gmail.com>
2022-12-04 14:18:55 +0100

    neovim: don't auto-select completion

--- users/iverberk/nvim/lua/plugins/nvim-cmp.lua
+++ users/iverberk/nvim/lua/plugins/nvim-cmp.lua
@@ -17,7 +17,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
-  {  
+  {
     { name = 'buffer' },
   }),
   window = {
@@ -43,8 +43,8 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
